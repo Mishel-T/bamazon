fd342e18e50075ac541fdc7b850f7ba9a1c5a998
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-07-11 09:21:20 -0700

    fixed db name when initialized and added fx to display all products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,11 +5,22 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "codE_banana1",
-  database: "playlist_db"
+  database: "bamazon"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    
+    readProducts();
   });
+
+  //function to display all products
+  function readProducts() {
+    console.log("Displaying all Products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
