cf83d872b82a47a31a115b24ad18121b1d4efcc9
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-07-14 00:02:05 -0700

    customer coded but has issues. Images of progress so far added to readMe

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bamazon
 Node.js &amp; MySQL
+
+bamazon is a store front from which you can select items for purchase. 
+Once bamazon is run, find the id of the item you would like to order and enter that id when prompted. Then, enter the quanity you would like to buy of the item. 
+bamazon will then either let you know if your item is in stock and provide you with an order total or notify you if there is not enough stock to fulfill your order. 
+
+![See it in Action](bamazon-1.png)
+![](bamazon-2.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,74 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "codE_banana1",
+  password: "mypw",
   database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    readProducts();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  readProducts();
+});
 
-  //function to display all products
-  function readProducts() {
-    console.log("Displaying all Products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  }
+//function to display all products
+function readProducts() {
+  console.log("Displaying all Products...\n");
+  connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    beginOrder()
+  });
+}
+
+//create function using inquirer to prompt user to (entry 1)select id and then (entry 2)select number of units they want to buy
+function beginOrder() {
+  inquirer
+    .prompt([
+      {
+        name: "ItemID",
+        type: "number",
+        message: "Enter the Item ID of the item you would like to purchase.",
+        //add code to prompt user to "Please enter a valid id" if an existing item id is not entered
+      },
+      {
+        name: "Quantity",
+        type: "number",
+        message: "How many units of this product would you like to purchase?"
+      }
+    ])
+        .then(function (answer) {
+          //compare entry 2 to stock_quantitiy - to do that, have to find item based on id
+          var query = "SELECT stock_quantity, product_name FROM products WHERE ?";
+          connection.query(query, {item_id: answer.ItemID}, function (err, res) {
+            if (err) throw err;            
+            console.log(res);
+            //this line not working - returning undefined
+            console.log(res.stock_quantity + " " + res.product_name + " left in stock");
+            
+
+            //if entry 2 > stock_quantity - console.log Insufficient quantity!
+            //console.log Order Total: <price>
+            //if (answer.Quantity > res.stock_quantity) {
+              //console.log("Insufficient Quantity!")
+           // }
+            //else update products db to update stock_quantity-entry 2
+
+          //}
+          connection.end();
+
+          });
+        })
+
+   
+
+
+}
+
+
